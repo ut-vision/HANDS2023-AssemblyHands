8bdf13e58a483a87a73a9850a83af2816789f7a1
tkhkaeio <tkhkaeio121@gmail.com>
2023-08-26 23:56:05 +0200

    update v1-3

--- challenge_submit.py
+++ challenge_submit.py
@@ -3,9 +3,10 @@ import json
 
 # NOTE: the number of samples in each test sequence.
 # Please check the number before your submition.
+version = "v1-3"
 test_seq_stats = {
-    "nusar-2021_action_both_9034-c08b_9034_user_id_2021-02-04_161726": 8332,
-    "nusar-2021_action_both_9021-c10a_9021_user_id_2021-02-23_100458": 7501,
+    "nusar-2021_action_both_9034-c08b_9034_user_id_2021-02-04_161726": 8309,
+    "nusar-2021_action_both_9021-c10a_9021_user_id_2021-02-23_100458": 7461,
     "nusar-2021_action_both_9061-c13d_9061_user_id_2021-02-09_143830": 3192,
     "nusar-2021_action_both_9074-a29_9074_user_id_2021-02-11_154856": 6923,
 }
@@ -19,7 +20,7 @@ def get_pred(img=None, camera_intrinsic=None, camera_extrinsic=None):
     xyz = np.zeros((42, 3))
     return xyz
 
-with open('template/assemblyhands_test_joint_3d_iccv23_pred_template.json', 'r') as f:
+with open(f'template/assemblyhands_test_joint_3d_iccv23-{version}_pred_template.json', 'r') as f:
     kpt_pred = json.load(f) # fill in -1 by default
 
 total_samples = 0
